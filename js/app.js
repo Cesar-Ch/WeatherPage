f9b6f01243c5007786353d7529519d72f1b4d19d
Cesar Ch <chafloque1530@gmail.com>
2022-11-23 14:45:23 -0500

    Updates

--- js/app.js
+++ js/app.js
@@ -1,35 +1,22 @@
 window.addEventListener("load", () => {
   let lon;
   let lat;
-
   let temperaturaValor = document.getElementById("temperatura-valor");
-  // let temperaturaDescripcion = document.getElementById('temperatura-descripcion');
-
   let ubicacion = document.getElementById("ubicacion");
   let icono = document.getElementById("icono");
-
   let vientoVelocidad = document.getElementById("viento-velocidad");
-
   let pais = document.getElementById("pais");
-
   let humidity = document.getElementById("humidity");
-
   let visibility = document.getElementById("visibility");
-
   let pressure = document.getElementById("pressure");
-
   let body = document.getElementById("body");
-
   const form = document.getElementById("search-form");
   const searchBox = document.getElementById("search");
 
   if (navigator.geolocation) {
-    console.log(navigator);
-    console.log(navigator.geolocation);
     navigator.geolocation.getCurrentPosition((posicion) => {
       lon = posicion.coords.longitude;
       lat = posicion.coords.latitude;
-
       const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=9363411c6c37e902fe367c563c619623`;
       console.log(url);
       fetch(url)
@@ -40,12 +27,13 @@ window.addEventListener("load", () => {
           console.log(data);
           let temp = Math.round(data.main.temp);
           temperaturaValor.textContent = `${temp - 273}°C`;
-          ubicacion.textContent = data.name;
-          vientoVelocidad.textContent = `${(data.wind.speed * 3, 6)} Km/h`;
+          ubicacion.textContent = data.name + ",";
+          vientoVelocidad.textContent = `${Math.floor(data.wind.speed * 3.6)} Km/h`;
           pais.textContent = data.sys.country;
           humidity.textContent = `${data.main.humidity}%`;
           visibility.textContent = `${data.visibility / 1000} Km`;
-          pressure.textContent = `${data.main.pressure} hPa`;
+          pressure.textContent = `${data.main.pressure} mbar`;
+          console.log(data.weather[0].icon)
           switch (data.weather[0].icon) {
             case "01n":
               icono.src = "img/animated/night.svg";
@@ -60,27 +48,27 @@ window.addEventListener("load", () => {
             case "03n":
               icono.src = "img/animated/cloudy.svg";
               body.style.background =
-                "linear-gradient(to right top, #757f9a, #d7dde8)";
+                "linear-gradient(to right top, #1B356E, #4783C1)";
               break;
             case "04n":
               icono.src = "img/animated/cloudy.svg";
               body.style.background =
-                "linear-gradient(to right top, #757f9a, #d7dde8)";
+                "linear-gradient(to right top, #1B356E, #4783C1)";
               break;
             case "09n":
               icono.src = "img/animated/rainy-6.svg";
               body.style.background =
-                "linear-gradient(to right top, #757f9a, #d7dde8)";
+                "linear-gradient(to right top, #1B356E, #4783C1)";
               break;
             case "10n":
               icono.src = "img/animated/rainy-6.svg";
               body.style.background =
-                "linear-gradient(to right top, #757f9a, #d7dde8)";
+                "linear-gradient(to right top, #1B356E, #4783C1)";
               break;
             case "11n":
               icono.src = "img/animated/thunder.svg";
               body.style.background =
-                "linear-gradient(to right top, #757f9a, #d7dde8)";
+                "linear-gradient(to right top, #1B356E, #4783C1)";
               break;
             case "13n":
               icono.src = "img/animated/snowy-6.svg";
@@ -100,27 +88,27 @@ window.addEventListener("load", () => {
             case "03d":
               icono.src = "img/animated/cloudy.svg";
               body.style.background =
-                "linear-gradient(to right top, #757f9a, #d7dde8)";
+                "linear-gradient(to right top, #1B356E, #4783C1)";
               break;
             case "04d":
               icono.src = "img/animated/cloudy.svg";
               body.style.background =
-                "linear-gradient(to right top, #757f9a, #d7dde8)";
+                "linear-gradient(to right top, #1B356E, #4783C1)";
               break;
             case "09d":
               icono.src = "img/animated/rainy-6.svg";
               body.style.background =
-                "linear-gradient(to right top, #757f9a, #d7dde8)";
+                "linear-gradient(to right top, #1B356E, #4783C1)";
               break;
             case "10d":
               icono.src = "img/animated/rainy-3.svg";
               body.style.background =
-                "linear-gradient(to right top, #757f9a, #d7dde8)";
+                "linear-gradient(to right top, #1B356E, #4783C1)";
               break;
             case "11d":
               icono.src = "img/animated/thunder.svg";
               body.style.background =
-                "linear-gradient(to right top, #757f9a, #d7dde8)";
+                "linear-gradient(to right top, #1B356E, #4783C1)";
               break;
             case "13d":
               icono.src = "img/animated/snowy-6.svg";
@@ -138,22 +126,23 @@ window.addEventListener("load", () => {
     event.preventDefault();
     search(searchBox.value);
   }
-
   form.addEventListener("submit", onSubmit);
-
   function search(local) {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${local}&appid=9363411c6c37e902fe367c563c619623`;
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
-        ubicacion.innerHTML = data.name;
+        console.log(data);
+        console.log(data.visibility);
+        ubicacion.innerHTML = data.name + ",";
         pais.innerHTML = data.sys.country;
         temperaturaValor.innerHTML = `${Math.ceil(data.main.temp - 273.15)}°C`;
-        vientoVelocidad.innerHTML = `${(data.wind.speed * 3, 6)} Km/h`;
+        vientoVelocidad.innerHTML = `${Math.floor(data.wind.speed * 3.6)} Km/h`;
         humidity.innerHTML = `${data.main.humidity}%`;
-        visibility.innerHTML = `${data.visibility / 1000} Km`;
-        pressure.innerHTML = `${data.main.pressure} hPa`;
+        visibility.innerHTML = `${(data.visibility / 1000)} Km`;
+        pressure.innerHTML = `${data.main.pressure} mbar`;
         icono.innerHTML = data.weather[0].icon;
+        searchBox.value = "";
         console.log(data.weather[0].icon);
         switch (data.weather[0].icon) {
           case "01n":
@@ -169,27 +158,27 @@ window.addEventListener("load", () => {
           case "03n":
             icono.src = "img/animated/cloudy.svg";
             body.style.background =
-              "linear-gradient(to right top, #757f9a, #d7dde8)";
+              "linear-gradient(to right top, #1B356E, #4783C1)";
             break;
           case "04n":
             icono.src = "img/animated/cloudy.svg";
             body.style.background =
-              "linear-gradient(to right top, #757f9a, #d7dde8)";
+              "linear-gradient(to right top, #1B356E, #4783C1)";
             break;
           case "09n":
             icono.src = "img/animated/rainy-6.svg";
             body.style.background =
-              "linear-gradient(to right top, #757f9a, #d7dde8)";
+              "linear-gradient(to right top, #1B356E, #4783C1)";
             break;
           case "10n":
             icono.src = "img/animated/rainy-6.svg";
             body.style.background =
-              "linear-gradient(to right top, #757f9a, #d7dde8)";
+              "linear-gradient(to right top, #1B356E, #4783C1)";
             break;
           case "11n":
             icono.src = "img/animated/thunder.svg";
             body.style.background =
-              "linear-gradient(to right top, #757f9a, #d7dde8)";
+              "linear-gradient(to right top, #1B356E, #4783C1)";
             break;
           case "13n":
             icono.src = "img/animated/snowy-6.svg";
@@ -209,27 +198,27 @@ window.addEventListener("load", () => {
           case "03d":
             icono.src = "img/animated/cloudy.svg";
             body.style.background =
-              "linear-gradient(to right top, #757f9a, #d7dde8)";
+              "linear-gradient(to right top, #1B356E, #4783C1)";
             break;
           case "04d":
             icono.src = "img/animated/cloudy.svg";
             body.style.background =
-              " linear-gradient(to right top, #757f9a, #d7dde8) ";
+              " linear-gradient(to right top, #1B356E, #4783C1) ";
             break;
           case "09d":
             icono.src = "img/animated/rainy-6.svg";
             body.style.background =
-              "linear-gradient(to right top, #757f9a, #d7dde8)";
+              "linear-gradient(to right top, #1B356E, #4783C1)";
             break;
           case "10d":
             icono.src = "img/animated/rainy-3.svg";
             body.style.background =
-              "linear-gradient(to right top, #757f9a, #d7dde8)";
+              "linear-gradient(to right top, #1B356E, #4783C1)";
             break;
           case "11d":
             icono.src = "img/animated/thunder.svg";
             body.style.background =
-              "linear-gradient(to right top, #757f9a, #d7dde8)";
+              "linear-gradient(to right top, #1B356E, #4783C1)";
             break;
           case "13d":
             icono.src = "img/animated/snowy-6.svg";
@@ -242,4 +231,5 @@ window.addEventListener("load", () => {
         console.log(error);
       });
   }
+  
 });
